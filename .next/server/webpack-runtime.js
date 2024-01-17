0e6f9c85404d13ea7229e83b5c11388920362013
JEaLiFe Jeaguy <jeanguylanememiaghe@gmail.com>
2024-01-17 23:43:21 +0000

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   .next/app-build-manifest.json
    	modified:   .next/cache/webpack/client-development/1.pack.gz
    	modified:   .next/cache/webpack/client-development/3.pack.gz
    	modified:   .next/cache/webpack/client-development/5.pack.gz
    	new file:   .next/cache/webpack/client-development/6.pack.gz
    	new file:   .next/cache/webpack/client-development/7.pack.gz
    	new file:   .next/cache/webpack/client-development/8.pack.gz
    	modified:   .next/cache/webpack/client-development/index.pack.gz
    	modified:   .next/cache/webpack/client-development/index.pack.gz.old
    	modified:   .next/cache/webpack/server-development/2.pack.gz
    	modified:   .next/cache/webpack/server-development/3.pack.gz
    	modified:   .next/cache/webpack/server-development/6.pack.gz
    	new file:   .next/cache/webpack/server-development/7.pack.gz
    	new file:   .next/cache/webpack/server-development/8.pack.gz
    	new file:   .next/cache/webpack/server-development/9.pack.gz
    	modified:   .next/cache/webpack/server-development/index.pack.gz
    	modified:   .next/cache/webpack/server-development/index.pack.gz.old
    	modified:   .next/server/app-paths-manifest.json
    	modified:   .next/server/app/page_client-reference-manifest.js
    	modified:   .next/server/app/patisserie/page_client-reference-manifest.js
    	modified:   .next/server/app/programme/page.js
    	modified:   .next/server/app/programme/page_client-reference-manifest.js
    	modified:   .next/server/app/reconnect.me/page_client-reference-manifest.js
    	modified:   .next/server/vendor-chunks/next.js
    	modified:   .next/server/webpack-runtime.js
    	modified:   .next/static/chunks/app/layout.js
    	modified:   .next/static/chunks/webpack.js
    	new file:   .next/static/webpack/29199a612fa34168.webpack.hot-update.json
    	new file:   .next/static/webpack/3313ec3f54f08020.webpack.hot-update.json
    	new file:   .next/static/webpack/6b755b26d30b1b3e.webpack.hot-update.json
    	new file:   .next/static/webpack/9a3f1a4cf48adab5.webpack.hot-update.json
    	new file:   .next/static/webpack/app/layout.29199a612fa34168.hot-update.js
    	new file:   .next/static/webpack/app/layout.3313ec3f54f08020.hot-update.js
    	new file:   .next/static/webpack/app/layout.6b755b26d30b1b3e.hot-update.js
    	new file:   .next/static/webpack/app/layout.9a3f1a4cf48adab5.hot-update.js
    	new file:   .next/static/webpack/webpack.29199a612fa34168.hot-update.js
    	new file:   .next/static/webpack/webpack.3313ec3f54f08020.hot-update.js
    	new file:   .next/static/webpack/webpack.6b755b26d30b1b3e.hot-update.js
    	new file:   .next/static/webpack/webpack.9a3f1a4cf48adab5.hot-update.js
    	modified:   .next/trace
    	modified:   src/app/programme/page.jsx

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -47,11 +47,6 @@
 /******/ 	__webpack_require__.m = __webpack_modules__;
 /******/ 	
 /************************************************************************/
-/******/ 	/* webpack/runtime/amd options */
-/******/ 	(() => {
-/******/ 		__webpack_require__.amdO = {};
-/******/ 	})();
-/******/ 	
 /******/ 	/* webpack/runtime/compat get default export */
 /******/ 	(() => {
 /******/ 		// getDefaultExport function for compatibility with non-harmony modules
@@ -130,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("f69fcb34f1df0922")
+/******/ 		__webpack_require__.h = () => ("3e727c19dfe6d14b")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- src/app/programme/page.jsx
+++ src/app/programme/page.jsx
@@ -3,20 +3,16 @@ import design from '../../../public/assets/images/design.png'
 import Image from 'next/image'
 export default function page() {
   return (
-    <main  className="flex min-h-dvh  flex-col items-center justify-between p-24 md:pt-24 md:px-5 bg-white">
+    <main  className="flex min-h-dvh  flex-col items-center justify-center p-24 md:pt-24 md:px-5 bg-white">
 
       <div className="max-w-5xl w-full items-center justify-center font-mono text-sm flex flex-col gap-3">
 
-        <div className="content py-2 grid grid-cols-1 sm:grid-cols-1 items-center gap-4">
+        <div className="content py-2 flex h-full items-center gap-4">
           <Image
             src={design}
             width={400}
             alt="Gif"
           />
-          {/* <p className='text-slate-900 text-xl ' >
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt, enim voluptatum. Officiis excepturi quo est vitae in praesentium temporibus consectetur!
-
-          </p> */}
         </div>
       </div>
 
