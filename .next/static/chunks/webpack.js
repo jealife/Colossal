2fe1ffb5b0303c6a1f40ceb207fda7ccd93c78e5
JEaLiFe Jeaguy <jeanguylanememiaghe@gmail.com>
2024-01-19 03:14:22 +0000

    modified:   .next/app-build-manifest.json 	modified:   .next/server/app-paths-manifest.json 	modified:   .next/server/app/page.js 	modified:   .next/server/app/page_client-reference-manifest.js 	modified:   .next/server/vendor-chunks/next.js 	modified:   .next/server/webpack-runtime.js 	modified:   .next/static/chunks/app/layout.js 	modified:   .next/static/chunks/webpack.js 	new file:   .next/static/webpack/7e0099fc558ec6c8.webpack.hot-update.json 	new file:   .next/static/webpack/app/layout.7e0099fc558ec6c8.hot-update.js 	new file:   .next/static/webpack/webpack.7e0099fc558ec6c8.hot-update.js 	modified:   .next/trace 	modified:   src/components/navigation/Nav.jsx

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7e0099fc558ec6c8"; }
+/******/ 		__webpack_require__.h = function() { return "40212a13eae4deb8"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/components/navigation/Nav.jsx
+++ src/components/navigation/Nav.jsx
@@ -51,8 +51,6 @@ function MobileNav({ open, setOpen }) {
                     <svg onClick={() => setOpen(!open)} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#E5E5E5" className="w-8 h-8 cursor-pointer">
                         <path fillRule="evenodd" d="M5.47 5.47a.75.75 0 0 1 1.06 0L12 10.94l5.47-5.47a.75.75 0 1 1 1.06 1.06L13.06 12l5.47 5.47a.75.75 0 1 1-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 0 1-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 0 1 0-1.06Z" clipRule="evenodd" />
                     </svg>
-
-
                 </div>
             </div>
 
