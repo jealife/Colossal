78152d4ecf04c33fc32ff2e6594c49d8e5aa1c66
JEaLiFe Jeaguy <jeanguylanememiaghe@gmail.com>
2024-01-20 17:57:49 +0000

    Changes to be committed: 	modified:   .next/cache/webpack/client-development/13.pack.gz 	modified:   .next/cache/webpack/client-development/index.pack.gz 	modified:   .next/cache/webpack/client-development/index.pack.gz.old 	modified:   .next/cache/webpack/server-development/3.pack.gz 	modified:   .next/cache/webpack/server-development/index.pack.gz 	modified:   .next/cache/webpack/server-development/index.pack.gz.old 	modified:   src/app/patisserie/page.jsx

--- src/app/patisserie/page.jsx
+++ src/app/patisserie/page.jsx
@@ -31,7 +31,7 @@ export default function page() {
       <div className="annoce px-24 sm:px-[20px] ">
         <div className="annonc-wrapper w-[400px] md:w-auto shadow-lg p-3 rounded-md -mt-24 bg-white flex flex-col gap-3 items-center text-center">
           <p className='text-slate-900'>
-          Les précommandes se font sous 24h/72h à l’avance. <br />
+            Les précommandes se font sous 24h/72h à l’avance. <br />
             Réserves ton dessert dès maintenant et
             reçois ton formulaire pour personnaliser ta commande.
           </p>
@@ -64,6 +64,9 @@ export default function page() {
           <Gallery>
             <Crepres />
           </Gallery>
+        </Tab>
+
+        <Tab label="Layer cake">
         </Tab>
 
       </Tabs>
