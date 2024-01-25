17024c4fe52652d0609f2a9225b4a6315ad8d407
JEaLiFe <83034287+jealife@users.noreply.github.com>
2024-01-25 20:39:34 +0000

    Changes to be committed: 	modified:   src/app/layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -18,7 +18,12 @@ export const metadata = {
   openGraph: {
     icon: '/logo.png',
     images: 'https://images.unsplash.com/photo-1611497406092-4bc22c54b322?q=80&w=1934&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    url:'https://colossal-mindand-cake.vercel.app/',
   },
+  twitter:{
+    
+    card:'summary_large_image'
+  }
 }
 
 export default function RootLayout({ children }) {
